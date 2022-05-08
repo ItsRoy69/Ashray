d46801c1966e0e2a7ad2d8a7b15b1f1f3542ca30
Tamal Das <gyansujan69@gmail.com>
2022-05-08 13:13:18 +0530

    Fixed minor bugs

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb+srv://tamaldas69:tamaldas69@cluster0.g1f0o.mongodb.net/milandatabase?retryWrites=true&w=majority";
+  "mongodb+srv://tamaldas69:tamaldas69@cluster0.oj0rd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
